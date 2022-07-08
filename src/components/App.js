74051d15f7098f8171242460efb317d16ac50360
Alain Noel <alain.noel34@gmail.com>
2022-07-08 19:58:33 +0200

    input ok

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import Banner from './Banner'
 // import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import logo from '../assets/logo.png'
-import handleSubmit from './QuestionForm'
+import QuestionForm from './QuestionForm'
 
 function App() {
     return (
@@ -12,7 +12,7 @@ function App() {
             <h1 className='lmj-title'>La maison jungle</h1>
           </Banner>
           {/*<Cart /> */}
-              <form onSubmit={handleSubmit}>
+              <form onSubmit={QuestionForm}>
                   <input type='text' name='my_input' defaultValue='Tapez votre texte' />
                   <button type='submit'>Entrer</button>
               </form>
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,16 @@
-function handleSubmit(e){
-    e.preventDefault()
-    alert(e.target['my_input'].value)
+import { useState } from 'react'
+
+function QuestionForm(){
+    const [inputValue, setInputValue] = useState('Posez votre question ici')
+    return (
+      <div>
+          <textarea
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+      </div>
+    )
 }
 
-export default handleSubmit
+export default QuestionForm
